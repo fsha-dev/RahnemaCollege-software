8243481d66c58a2155b62338337af86d32ddc25d
Faeze <faezeh.shabannejad@gmail.com>
2025-08-12 15:29:52 +0330

    layout sign up and login added
    -

---
+++ project/pr1/src/components/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,10 @@
+import Modal from "../../components/Modal/Modal";
+function AuthLayout() {
+  return (
+    <section className="bg-[url(./assets/loginSign/heroImage.jpg)] w-full h-screen bg-cover  bg-center bg-no-repeat flex justify-center items-center">
+      <Modal />
+    </section>
+  );
+}
+
+export default AuthLayout;
---
+++ project/pr1/src/components/Modal/Modal.tsx
@@ -0,0 +1,7 @@
+import "../../assets/variables.css";
+function Modal() {
+  return (
+    <div className="w-1/3 h-[62vh]  rounded-[2.5rem] bg-[var(--Background-white)] shadow-[var(--shadow-black)]"></div>
+  );
+}
+export default Modal;
